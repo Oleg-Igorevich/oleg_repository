39dcb61f366032d5ad6e13f03098f618b3eab36a
Oleg Gladkov <ogladkov@BPA.LOCAL>
2019-07-10 18:48:34 -0400

    whaaa

---
+++ hashtag-data-science-class-week2.R
@@ -0,0 +1,12 @@
+print("This is a file that I want to commit...and possibly push to my Git repository")
+
+
+# staging -----------------------------------------------------------------
+
+# git clone [paste link to online repository] ;creates a copy of the repository folder locally
+# cd [name of repository] ;sets your working directory to that local copy of the repository
+# cd ;on its own, changes it to your working directory
+
+# add -A ;says that all changes (-A) will be committed
+# commit 
+# 
